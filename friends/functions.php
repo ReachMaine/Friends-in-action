d63d8da477d0a87ec09d687f0f54f35f66e1f223
Linda Speight <linda@reachmaine.com>
2016-11-10 15:52:11 -0500

    Topbar menu
    add menu/nav area
    add topbar & put nav
    do some styling of it.

--- friends/functions.php
+++ friends/functions.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 require_once(get_stylesheet_directory().'/custom/woocommerce.php');
 require_once(get_stylesheet_directory().'/custom/language.php'); */
 
@@ -7,12 +7,12 @@ require_once(get_stylesheet_directory().'/custom/language.php'); */
 	/* add favicons for admin */
 	/*add_action('login_head', 'add_favicon');
 	add_action('admin_head', 'add_favicon');
-	
+
 	function add_favicon() {
 		$favicon_url = get_stylesheet_directory_uri() . '/images/admin-favicon.ico';
 		echo '<link rel="shortcut icon" href="' . $favicon_url . '" />';
 	} */
-	
+
 	/***** end admin favicon *****/
 	/*****  change the login screen logo ****/
 	/* size should be somewhere around 300 x 60 */
@@ -21,7 +21,7 @@ require_once(get_stylesheet_directory().'/custom/language.php'); */
 			body.login div#login h1 a {
 				background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/admin-login-logo.png);
 				padding-bottom: 30px;
-				background-size: contain; 
+				background-size: contain;
 				margin-left: 0px;
 				margin-bottom: 0px;
 				margin-right: 0px;
@@ -32,11 +32,11 @@ require_once(get_stylesheet_directory().'/custom/language.php'); */
 	<?php }
 	add_action( 'login_enqueue_scripts', 'my_login_logo' );
 	/*****  end custom login screen logo ****/
-	
+
 	add_action('after_setup_theme', 'rEAch_setup');
 	/**  ea_setup
 	*  init stuff that we have to init after the main theme is setup.
-	* 
+	*
 	*/
 	function rEAch_setup() {
 	 /* do stuff here. */
@@ -47,5 +47,11 @@ require_once(get_stylesheet_directory().'/custom/language.php'); */
 	function wrap_embed_with_div($html, $url, $attr) {
 	        return '<div class="ea-responsive-container">'.$html.'</div>';
 	}
+
+	/* add menu location */
+	function register_top_menu() {
+	  	register_nav_menu('top-menu',__( 'Top Bar Menu' ));
+	}
+	add_action( 'init', 'register_top_menu' );
 
 ?>
